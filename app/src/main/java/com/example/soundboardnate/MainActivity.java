3b081a9a78c21b00e2e62a8939eb474cb151aa71
201158870 <nathanberumen25@gmail.com>
2023-06-05 14:01:53 -0700

    okay

--- app/src/main/java/com/example/soundboardnate/MainActivity.java
+++ app/src/main/java/com/example/soundboardnate/MainActivity.java
@@ -2,13 +2,150 @@ package com.example.soundboardnate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button  alarm;
+    MediaPlayer mp;
+
+
+    private Button  birds;
+
+    private Button  dogbark;
+    private Button  gasp;
+    private Button  laser;
+    private Button  laugh;
+    private Button  rain;
+    private Button  retrogame;
+    private Button  rocket;
+    private Button  sadtrombone;
+    private Button  sneeze;
+    private Button  swoosh;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alarm = findViewById(R.id.button);
+        birds = findViewById(R.id.button2);
+        dogbark = findViewById(R.id.button3);
+        gasp = findViewById(R.id.button4);
+        laser = findViewById(R.id.button5);
+        laugh = findViewById(R.id.button6);
+        rain = findViewById(R.id.button7);
+        retrogame = findViewById(R.id.button8);
+        rocket = findViewById(R.id.button9);
+        sadtrombone = findViewById(R.id.button10);
+        sneeze = findViewById(R.id.button11);
+        swoosh = findViewById(R.id.button12);
+
+        alarm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_alarm);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+        birds.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_birds);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+
+
+        dogbark.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_dogbark);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+
+        gasp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_girlgasp);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+
+        laser.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_laser);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+
+        laugh.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_laugh);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+
+        rain.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_rain);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+
+        retrogame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_retrogame);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+
+        rocket.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_rocket);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+
+        sadtrombone.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mp = MediaPlayer.create(MainActivity.this,R.raw.sample_nate_sadtrombone);
+                mp.start();
+                // Code here executes on main thread after user presses button
+            }
+
+
+        });
+
+
     }
 }
